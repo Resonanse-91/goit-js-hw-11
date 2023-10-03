b3264af65fbf24afbb9da62d9b482ab36438d4d9
Resonanse-91 <129589540+Resonanse-91@users.noreply.github.com>
2023-10-03 13:32:00 +0300

    js-hw-11

--- src/fetch.js
+++ src/fetch.js
@@ -9,4 +9,22 @@ async function fetchImg(searchQuery, page, perPage) {
   );
   return response.data;
 }
+
+async function fetchImages(name) {
+  const options = {
+            params: {
+                key: '39792368-ef45e8f9e60c37d780e3d3bf1',
+                q: name ,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: 'true',
+                page: page,
+                per_page: 40,
+            },
+        }
+        const baseUrl = 'https://pixabay.com/api/';
+        const resp = await axios.get(baseUrl, options)
+    return resp
+    }   
+
 export { fetchImg };
--- src/index.js
+++ src/index.js
@@ -47,22 +47,7 @@ function getImages(evt) {
              })
         } 
     } 
-  async function fetchImages(name) {
-  const options = {
-            params: {
-                key: '39125221-1f7d496f02856d1b2d986a210',
-                q: name ,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: 'true',
-                page: page,
-                per_page: 40,
-            },
-        }
-        const baseUrl = 'https://pixabay.com/api/';
-        const resp = await axios.get(baseUrl, options)
-    return resp
-    }   
+    
 
 function handlerLoadMore(entries) {
   entries.forEach((entry) => {
