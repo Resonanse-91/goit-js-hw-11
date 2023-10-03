c6f6e33488b33b42f93fd2ee742842d6b5f5b655
Resonanse-91 <129589540+Resonanse-91@users.noreply.github.com>
2023-10-03 12:11:16 +0300

    js-hw-11

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -47,22 +46,7 @@ function getImages(evt) {
              })
         } 
     } 
-  async function fetchImages(name) {
-  const options = {
-            params: {
-                key: '39125221-1f7d496f02856d1b2d986a210',
-                q: name ,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: 'true',
-                page: page,
-                per_page: 40,
-            },
-        }
-        const baseUrl = 'https://pixabay.com/api/';
-        const resp = await axios.get(baseUrl, options)
-    return resp
-    }   
+  
 
 function handlerLoadMore(entries) {
   entries.forEach((entry) => {
